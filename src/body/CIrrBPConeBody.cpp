c8f24c9ae7172e9ad999c9aae6cf6b7a20c6c66e
stefanoguerrini93 <stefanoguerrini93@gmail.com>
2010-09-02 19:48:58 +0000

    + Added body orientation support to cone body + Doxygen documentation renewed

--- src/body/CIrrBPConeBody.cpp
+++ src/body/CIrrBPConeBody.cpp
@@ -10,16 +10,61 @@ CIrrBPConeBody::CIrrBPConeBody(ISceneNode * node,irr::f32 mass, irr::s32 bodyId,
    vector3df TScale = node->getScale();
 
    
+   vector3df Extent;
+   Extent = node->getBoundingBox().getExtent();
+
    irr::f32 radius,height;
-   radius = fabs(edges[4].X-edges[0].X)/2.0f;
-   height = fabs(edges[1].Y-edges[0].Y);
-
-   radius *= TScale.X;
-   height *= TScale.Y;
-
    m_MotionState = new CMotionState(this,getTransformFromIrrlichtNode(node));
 
-   m_Shape = new btConeShape(radius,height);
+   BODY_OR coneOr = bodyOrientationAxis;
+
+   if(coneOr == AUTO)
+   {
+   		   if(Extent.X == Extent.Y && Extent.X == Extent.Z) //not recognizable. so take default axis
+				coneOr = Y;
+		   else if(Extent.Y == Extent.Z)
+				coneOr =  X;
+		   else if(Extent.X == Extent.Z)
+				coneOr = Y;
+		   else if(Extent.X == Extent.Y)
+				coneOr = Z;
+		   else
+				coneOr = Y;
+   }
+
+   switch(coneOr)
+   {
+		case X:
+			radius = fabs(edges[1].Y-edges[0].Y)/2.0f;
+			height = fabs(edges[4].X-edges[0].X);
+
+			radius *= TScale.Y;
+			height *= TScale.X;
+
+			m_Shape = new btConeShapeX(radius,height);
+			break;
+		case Y:
+
+		    radius = fabs(edges[4].X-edges[0].X)/2.0f;
+			height = fabs(edges[1].Y-edges[0].Y);
+
+			radius *= TScale.X;
+			height *= TScale.Y;
+			
+			m_Shape = new btConeShape(radius,height);
+			break;
+		case Z:
+
+			radius = fabs(edges[4].X-edges[0].X)/2.0f;
+			height = fabs(edges[2].Z-edges[0].Z);
+
+			radius *= TScale.X;
+			height *= TScale.Z;
+
+			m_Shape = new btConeShapeZ(radius,height);
+			break;
+   }
+  
    
    btVector3 LocalInertia;
    m_Shape->calculateLocalInertia(mass, LocalInertia);
